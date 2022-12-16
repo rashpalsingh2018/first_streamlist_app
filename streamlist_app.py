076d819942d9e2355da87ecc077b2bc13da3930d
rashpalsingh2018 <43048574+rashpalsingh2018@users.noreply.github.com>
2022-12-16 17:19:32 -0600

    Update streamlist_app.py
    added menu items

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3')
+streamlit.text('Kale')
+streamlit.text('Hard Boiled')
