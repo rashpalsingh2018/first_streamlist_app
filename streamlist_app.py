b5006b761fd11611ec524ef2d02fd96b65e9369f
rashpalsingh2018 <43048574+rashpalsingh2018@users.noreply.github.com>
2022-12-16 17:30:19 -0600

    Update streamlist_app.py
    added pandas

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title(' ":heart:" My Parents new healthy diner')
 streamlit.header('Breakfast menu')
@@ -6,3 +7,6 @@ streamlit.text('Omega 3')
 streamlit.text('Kale')
 streamlit.text('Hard Boiled')
 streamlit.header('Build your own smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
