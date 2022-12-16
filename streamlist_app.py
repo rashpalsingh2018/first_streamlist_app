a65a88cbdd697fe4ca58b231d66e847c9e6aeead
rashpalsingh2018 <43048574+rashpalsingh2018@users.noreply.github.com>
2022-12-16 17:25:11 -0600

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title(' ":heart:" My Parents new healthy diner')
 streamlit.header('Breakfast menu')
 streamlit.text('Omega 3')
 streamlit.text('Kale')
