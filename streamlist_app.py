fdd2dfa480c913610bd83218283f7d270681b155
rashpalsingh2018 <43048574+rashpalsingh2018@users.noreply.github.com>
2022-12-19 10:59:49 -0600

    Update streamlist_app.py
    separate fruit name from url

--- streamlist_app.py
+++ streamlist_app.py
@@ -21,8 +21,7 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruity vice api response
 streamlit.header('Fruityvice Fruit Advice!')
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # take json and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
